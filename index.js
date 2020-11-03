0e5042f4bcaa7e8824076856c88ffbc76639f5c8
Antizera <comar791@bk.ru>
2020-11-03 18:41:28 +0300

    add full

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //res.send('Hello World!')
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
